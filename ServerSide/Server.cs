f5bc8ff6f640d70e276b24e7618c429b0e6d7b4b
Thomas <tlindeker0@gmail.com>
2023-05-25 13:54:18 +0200

    test

--- ServerSide/Server.cs
+++ ServerSide/Server.cs
@@ -1,12 +1,14 @@
 ﻿using Lidgren.Network;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace ServerSide
 {
     class Server
     {
         private NetServer _server;
+        // create a dictionary all the players and a vector2 for their position
         private Dictionary<NetConnection, int> playerPositions = new Dictionary<NetConnection, int>();
 
         static void Main(string[] args)
@@ -45,6 +47,12 @@ namespace ServerSide
                                 playerPositions[message.SenderConnection]--;
                             else if (command == "MOVE_RIGHT")
                                 playerPositions[message.SenderConnection]++;
+                            else if (command == "MOVE_UP") 
+                                playerPositions[message.SenderConnection] += 10;
+                            else if (command == "MOVE_DOWN")
+                                playerPositions[message.SenderConnection] -= 10;
+                            else
+                                Console.WriteLine($"Unhandled command {command}");
                             break;
                         case NetIncomingMessageType.StatusChanged:
                             if ((NetConnectionStatus)message.ReadByte() == NetConnectionStatus.Disconnected)
@@ -61,7 +69,6 @@ namespace ServerSide
                         msg.Write(player.Key.RemoteUniqueIdentifier);
                         msg.Write(player.Value);
                         _server.SendMessage(msg, message.SenderConnection, NetDeliveryMethod.ReliableOrdered);
-                        Console.WriteLine("Sending position " + player.Value + " to " + player.Key.RemoteUniqueIdentifier);
                     }
                 }
             }
