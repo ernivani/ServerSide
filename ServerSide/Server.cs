a2e4b8350762a75abc2b8c67ae0944b683c330cf
Thomas Lindeker <thomas.lindeker@interieur.gouv.fr>
2023-06-02 16:36:51 +0200

    modification

--- ServerSide/Server.cs
+++ ServerSide/Server.cs
@@ -1,111 +1,52 @@
-﻿using Lidgren.Network;
-using System.Numerics;
+﻿using System;
+using Lidgren.Network;
+using System.Collections.Generic;
 
 namespace ServerSide
 {
-    class Server
+    class Program
     {
-        private NetServer _server;
-        private Dictionary<NetConnection, Vector2> playerPositions = new Dictionary<NetConnection, Vector2>();
-        private object positionsLock = new object(); // Lock for playerPositions
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting server...");
-            Console.WriteLine(args.Length);
-            var server = new Server();
-            server.Run();
-        }
-
-        private void Run()
-        {
-            var config = new NetPeerConfiguration("game");
-            config.Port = 9999;
-            config.MaximumConnections = 100;
-            config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
-
-            _server = new NetServer(config);
-            _server.Start();
-
+            NetPeerConfiguration config = new NetPeerConfiguration("game") { Port = 9999 };
+            NetServer server = new NetServer(config);
+            server.Start();
             Console.WriteLine("Server started");
 
-            // Start a new thread to handle incoming messages
-            Thread incomingMessageThread = new Thread(new ThreadStart(HandleIncomingMessages));
-            incomingMessageThread.Start();
+            Dictionary<long, (float, float)> playerPositions = new Dictionary<long, (float, float)>();
 
             while (true)
             {
-                lock (positionsLock)  // Lock the dictionary while iterating
+                NetIncomingMessage inc;
+                while ((inc = server.ReadMessage()) != null)
                 {
-                    foreach (var player in playerPositions)
+                    switch (inc.MessageType)
                     {
-                        if (player.Key.Status == NetConnectionStatus.Connected)
-                        {
-                            foreach (var otherPlayer in playerPositions)
-                            {
-                                var msg = _server.CreateMessage();
-                                msg.Write(otherPlayer.Key.RemoteUniqueIdentifier);
-                                msg.Write(otherPlayer.Value.X);
-                                msg.Write(otherPlayer.Value.Y);
-                                _server.SendMessage(msg, player.Key, NetDeliveryMethod.ReliableOrdered);
-                            }
-                        }
-                    }
-                }
-                Thread.Sleep(10);  // Give other threads a chance to run
-            }
-        }
-
-        private void HandleIncomingMessages()
-        {
-            while (true)
-            {
-                var message = _server.ReadMessage();
-
-                if (message != null)
-                {
-                    switch (message.MessageType)
-                    {
-                        case NetIncomingMessageType.ConnectionApproval:
-                            lock (positionsLock)
-                            {
-                                playerPositions[message.SenderConnection] = Vector2.Zero;
-                            }
-                            message.SenderConnection.Approve();
-                            break;
                         case NetIncomingMessageType.Data:
-                            string command = message.ReadString();
-                            lock (positionsLock)
+                            string message = inc.ReadString();
+                            if (message == "MOVE_TO")
                             {
-                                Console.WriteLine("Received command: " + command);
-                                if (command == "MOVE_LEFT")
-                                    playerPositions[message.SenderConnection] = new Vector2(playerPositions[message.SenderConnection].X - 1, playerPositions[message.SenderConnection].Y);
-                                else if (command == "MOVE_RIGHT")
-                                    playerPositions[message.SenderConnection] = new Vector2(playerPositions[message.SenderConnection].X + 1, playerPositions[message.SenderConnection].Y);
-                                else if (command == "MOVE_UP") 
-                                    playerPositions[message.SenderConnection] = new Vector2(playerPositions[message.SenderConnection].X, playerPositions[message.SenderConnection].Y - 1);
-                                else if (command == "MOVE_DOWN")
-                                    playerPositions[message.SenderConnection] = new Vector2(playerPositions[message.SenderConnection].X, playerPositions[message.SenderConnection].Y + 1);
-                                else if (command == "MOVE_TO")
+                                float x = inc.ReadFloat();
+                                float y = inc.ReadFloat();
+                                Console.WriteLine($"Player {inc.SenderConnection.RemoteUniqueIdentifier} moved to {x}, {y}");
+                                playerPositions[inc.SenderConnection.RemoteUniqueIdentifier] = (x, y);
+
+                                // Send updated player positions to all clients
+                                foreach (var connection in server.Connections)
                                 {
-                                    float x = message.ReadFloat();
-                                    float y = message.ReadFloat();
-                                    Console.WriteLine("Moving to: " + x + ", " + y);
-                                    Vector2 position = new Vector2(x, y);
-                                    playerPositions[message.SenderConnection] = position;
+                                    var outMessage = server.CreateMessage();
+                                    outMessage.Write(inc.SenderConnection.RemoteUniqueIdentifier);
+                                    outMessage.Write(x);
+                                    outMessage.Write(y);
+                                    server.SendMessage(outMessage, connection, NetDeliveryMethod.ReliableOrdered);
                                 }
                             }
                             break;
-                        case NetIncomingMessageType.StatusChanged:
-                            if ((NetConnectionStatus)message.ReadByte() == NetConnectionStatus.Disconnected)
-                            {
-                                lock (positionsLock)
-                                {
-                                    playerPositions.Remove(message.SenderConnection);
-                                }
-                            }
+
+                        default:
                             break;
                     }
+                    server.Recycle(inc);
                 }
             }
         }
